461865972a64643153ad1ad2ecc186e9e395ed6a
MikMosk86 <mimoal1986@mail.ru>
2026-02-02 01:43:30 +0300

    Update visit_calculator.py

--- visit_calculator.py
+++ visit_calculator.py
@@ -1,5 +1,5 @@
 # utils/visit_calculator.py
-# draft 1.8
+# draft 1.9
 import pandas as pd
 import numpy as np
 import streamlit as st
@@ -502,6 +502,7 @@ class VisitCalculator:
 
 # Глобальный экземпляр
 visit_calculator = VisitCalculator()
+
 
 
 

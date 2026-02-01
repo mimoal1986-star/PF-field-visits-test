b1b069aad17073d4fdecdfb9911b1ee5b5aa36ae
MikMosk86 <mimoal1986@mail.ru>
2026-02-02 01:44:15 +0300

    draft_1.9_data_cleaner.py

--- data_cleaner.py
+++ data_cleaner.py
@@ -1,5 +1,5 @@
 # utils/data_cleaner.py
-# draft 1.8
+# draft 1.9
 import pandas as pd
 import numpy as np
 from datetime import datetime, timedelta
@@ -1617,6 +1617,7 @@ class DataCleaner:
 
 # Глобальный экземпляр
 data_cleaner = DataCleaner()
+
 
 
 

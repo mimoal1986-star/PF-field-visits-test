8d0fe10363fe54321c10dbf188a566ecdadddd8c
MikMosk86 <mimoal1986@mail.ru>
2026-02-02 16:43:10 +0300

    Update visit_calculator.py

--- visit_calculator.py
+++ visit_calculator.py
@@ -22,6 +22,10 @@ class VisitCalculator:
             base['Имя клиента'] = field_projects_df['Имя клиента']
             base['Название проекта'] = field_projects_df['Название проекта']
             base['ПО'] = field_projects_df['ПО']
+            base['DSM'] = field_projects_df['ЗОД']
+            base['ASM'] = field_projects_df['АСС']
+            base['RS'] = field_projects_df['ЭМ рег']
+            base['Регион'] = field_projects_df['Регион']
             
             # 2. Добавляем колонки для дат (пока пустые)
             base['Дата старта'] = None
@@ -434,6 +438,7 @@ class VisitCalculator:
 
 # Глобальный экземпляр
 visit_calculator = VisitCalculator()
+
 
 
 
